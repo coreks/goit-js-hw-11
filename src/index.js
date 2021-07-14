c06450bd12b8f874d843f6d0b6a722349ec6834d
Aleksey Sobchenko <aleksobchenko@gmail.com>
2021-07-14 23:15:46 +0300

    feature chenge theme on click button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './sass/main.scss';
+import './js/theme.js';
+import './js/timer.js';
---
+++ src/js/theme.js
@@ -0,0 +1,30 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick() {
+  color = setInterval(() => {
+    return (refs.bodyEl.style.backgroundColor = getRandomHexColor());
+  }, 1000);
+
+  if (refs.btnStart) {
+    refs.btnStart.removeEventListener('click', onBtnStartClick);
+    refs.btnStart.style.opacity = '0.5';
+  }
+}
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(color);
+  if (refs.btnStop) {
+    refs.btnStart.addEventListener('click', onBtnStartClick);
+    refs.btnStart.style.opacity = '1';
+  }
+});
