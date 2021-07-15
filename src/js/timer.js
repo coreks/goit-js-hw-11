01a2988c654101c4140bda9fb417e99021c8f9fd
Aleksey Sobchenko <aleksobchenko@gmail.com>
2021-07-16 00:42:29 +0300

    feature timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,4 +1,48 @@
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
+
+// Swal.fire({
+//   title: 'Error!',
+//   text: 'Do you want to continue',
+//   icon: 'error',
+//   confirmButtonText: 'Cool',
+// });
+
+class Timer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = targetDate;
+    this.selector = selector;
+    // this.timerNode = document.querySelector(selector);
+    // console.log(this.timerNode);
+    this.refs = {
+      days: document.querySelector(`${this.selector},[data-days]`),
+      hours: document.querySelector(`${this.selector},[data-hours]`),
+      minutes: document.querySelector(`${this.selector},[data-minutes]`),
+      seconds: document.querySelector(`${this.selector},[data-seconds]`),
+    };
+  }
+
+  getMs() {
+    const targetDate = Date.parse(this.targetDate);
+    const currenDate = Date.now();
+    return targetDate - currenDate;
+  }
+
+  renderTimerData({ days, hours, minutes, seconds }) {
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = pad(hours);
+    this.refs.minutes.textContent = pad(minutes);
+    this.refs.seconds.textContent = pad(seconds);
+  }
+
+  timerStart() {
+    this.clearInterval = setInterval(() => {
+      const diffMs = this.getMs();
+      const { days, hours, minutes, seconds } = convertMs(diffMs);
+      console.log({ days, hours, minutes, seconds });
+      this.renderTimerData({ days, hours, minutes, seconds });
+    }, 1000);
+  }
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -19,23 +63,14 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
-
-Swal.fire({
-  title: 'Error!',
-  text: 'Do you want to continue',
-  icon: 'error',
-  confirmButtonText: 'Cool',
-});
-
-const refs = {
-  inputEl: document.getElementById('date-selector'),
-};
-
-refs.inputEl.addEventListener('input', onInputClick);
-
-function onInputClick(e) {
-  console.log(e.currentTarget);
+function pad(value) {
+  return String(value).padStart(2, '0');
 }
+
+const timer = new Timer({ selector: '#timer', targetDate: '2021, 07, 19' });
+
+// this.days = document.querySelector(`${selector} .value[data-value ='days']`);
+
+console.log(timer);
+timer.getMs();
+timer.timerStart();
